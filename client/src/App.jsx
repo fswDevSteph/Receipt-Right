92d0f3ecbe417017dee0de12f4080d318d7fd4fc
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-04-10 00:15:13 -0300

    added app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,75 @@
-// import { useState } from 'react'
-import camera from './components/camera/camera.jsx'
+import { useState } from 'react'
+import HeaderNav from './components/HeaderNav/HeaderNav.jsx';
+import ContactUs from './components/ContactUs/ContactUs.jsx';
+import FooterNav from './components/FooterNav/FooterNav.jsx';
+import ViewAllReceipts from './components/ViewAllReceipts/ViewAllReceipts.jsx';
+import CreateReceipt from './components/CreateReceipt/CreateReceipt.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import HomePage from './components/HomePage/HomePage.jsx';
+import LoginPage from './components/LoginPage/LoginPage.jsx';
+import LogoutPage from './components/LogoutPage/LogoutPage.jsx';
+import SettingsPage from './components/SettingsPage/SettingsPage.jsx';
+import RegistrationPage from './components/RegistrationPage/RegistrationPage.jsx';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { Outlet } from 'react-router-dom';
 
-import Headernav from './components/headernav/headernav.jsx'
 
 import './App.css'
 
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
-
-
   return (
-    <>
-      <Headernav />
-      <Camera />
-      <Upload />
-      <Footernav />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <RegistrationPage />
+
+
+
+
+        <LoginPage />
+        <HomePage />
+        <Dashboard />
+        <SettingsPage />
+
+        <CreateReceipt />
+        <ViewAllReceipts />
+        <SettingsPage />
+        <ContactUs />
+        <LogoutPage />
+        <FooterNav />
+
+
+
+      </>
+    </ApolloProvider>
   )
 }
 
