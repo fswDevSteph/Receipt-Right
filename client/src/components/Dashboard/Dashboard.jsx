bd3ad940cf06723efb4b2bf8525da9f1f4dde1e6
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-04-10 12:06:36 -0300

    dashboard jsx file

---
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react';
+import './Dashboard.css';
+import HeaderNav from '../HeaderNav/HeaderNav';
+import FooterNav from '../FooterNav/FooterNav';
+
+
+function Dashboard() {
+
+
+    return (
+        <div>
+            <HeaderNav />
+            <p>Dashboard Body</p>
+            {/* Code here */}
+
+            <FooterNav />
+        </div>
+    )
+}
+
+export default Dashboard;
