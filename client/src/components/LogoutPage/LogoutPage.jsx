a1a30665f549c22bf0dd0abb4aefa35b4e2c5247
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-04-10 12:10:17 -0300

    Registration, login jsx complete, logout files set up

---
+++ client/src/components/LogoutPage/LogoutPage.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react';
+import './LogoutPage.css';
+import HeaderNav from '../HeaderNav/HeaderNav';
+import FooterNav from '../FooterNav/FooterNav';
+
+
+function LogoutPage() {
+
+
+    return (
+        <div>
+            <HeaderNav />
+
+            {/* Code here */}
+            <p>LogoutPage Body</p>
+
+            <FooterNav />
+        </div>
+    )
+}
+
+export default LogoutPage;
