305cbe419421aeeb898cca089522cc1f94c16d94
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-04-13 12:36:01 -0300

    Styled homepage, import several images for development, added 'start free now' btn links to uploader'

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,13 +1,18 @@
 // import { useState } from 'react';
 import React from 'react';
 import './HomePage.css';
-import pexels2 from '../assets/icons/pexels2.jpg';
+// import pexels1 from '../assets/images/pexels1.jpg';  friends around table
+// import pexels2 from '../assets/images/pexels2.jpg'; family - chistmasy
+// import pexels3 from '../assets/images/pexels3.jpg'; lady jumping in red sack
+// import pexels4 from '../assets/images/pexels4.jpg'; tug of war black couple navy shirts
+import pexels5 from '../assets/images/pexels5.jpg';
+// import pexels6 from '../assets/images/pexels6.jpg'; interacial couple red shirts man in blackground
 // import logo from '../assets/icons/receipt_right_logo.png';
 
 import HeaderNav from '../HeaderNav/HeaderNav';
 import FooterNav from '../FooterNav/FooterNav';
 
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function HomePage() {
@@ -16,15 +21,29 @@ function HomePage() {
     return (
         <div>
             <HeaderNav />
-            <section className='hero'>
-                <h1>Receipt Right,</h1>
-                <h2>Save your time, not your receipts.</h2>
+            <div className="grid_container">
+                {/* <section> */}
+                <div className="branding_message">
+                    <h1>Receipt Right,</h1>
+                    <h2>
+                        Live a little more, worry a little less.
+                    </h2>
+                    <p>Save your time, not your receipts.</p>
+                    <Link to="/CreateReceipt">
+                        <button className='start_now_btn'>Start Free Now!</button>
+                    </Link>
+                </div>
 
+                <div className="branding_section_image">
+                    <img src={pexels5} alt="Happy Friends" className='happy_friends_image' />
+                </div>
 
+                {/* </section> */}
+            </div>
 
-                <img src={pexels2} alt="Happy Friends" className='happy_friends_image' />
+            <div className="addRecSection">
 
-            </section>
+            </div>
 
 
             <FooterNav />
